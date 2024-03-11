43bd4ec2174672db8b04603569bb64356f76b7d0
RobertoBrunofl <roberto.brunofl@gmail.com>
2024-03-10 22:36:32 -0300

    UserManager terminado por roberto

--- UserManager.js
+++ UserManager.js
@@ -1,29 +1,38 @@
 class UserManager {
   static #users = [];
   create(data) {
-    const user = {
-      id:
-        UserManager.#users.length === 0
-          ? 1
-          : UserManager.#users[UserManager.#users.length - 1].id + 1,
-      foto: data.foto,
-      email: data.email,
-      password: data.password,
-      role: 0,
-    };
-    UserManager.#users.push(user);
-    console.log("usuariocreado");
+    try {
+      const user = {
+        id:
+          UserManager.#users.length === 0
+            ? 1
+            : UserManager.#users[UserManager.#users.length - 1].id + 1,
+        foto: data.foto,
+        email: data.email,
+        password: data.password,
+        role: 0,
+      };
+      UserManager.#users.push(user);
+      console.log("Usuario creado");
+    } catch (error) {
+      console.error("Error al crear usuario:", error);
+    }
   }
-  read(){
-    return UserManager.#users
+  read() {
+    return UserManager.#users;
   }
 }
 
-const gestorDeUsuarios = new UserManager()
+const gestorDeUsuarios = new UserManager();
 gestorDeUsuarios.create({
-    foto : "roberto.jpg" ,
-    email : "roberto@gmail.com" ,
-    password : "hola123"
-}) // crear un nuevo usuario
+  foto: "roberto.jpg",
+  email: "roberto@gmail.com",
+  password: "hola123",
+});
+gestorDeUsuarios.create({
+  foto: "esteban.jpg",
+  email: "estaban@gmail.com",
+  password: "coder123",
+}); // crear un nuevo usuario
 
-console.log(gestorDeUsuarios.read())
+console.log(gestorDeUsuarios.read());
